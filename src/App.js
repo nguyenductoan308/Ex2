1c3003f9f5770d215ecca90e171d36e95e86d68b
Toan Nguyen <nguyenductoan308@gmail.com>
2022-07-24 14:37:54 +0700

    Ex2 ToDoList

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoItem from "./component/Todo_Item/todoItem";
+import TodoList from "./component/Todo_List/todoList";
+import './index.css';
+
+App.propTypes = {
+    
+};
+function App(props) {
+  return (
+    <div className='card' >
+      <TodoList/>
+      <TodoList/>
+      <TodoList/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Todo_List/todoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import TodoItem from '../Todo_Item/todoItem';
+import PropTypes from 'prop-types';
+import './style.css'
+
+TodoList.propTypes = {
+    
+};
+
+function TodoList(props) {
+    return (
+        <div className='card__group'>
+            <TodoItem title={'Task 1'} creator={'Toan'} desc={'This a task This a task This a task This a task..'} />
+            <TodoItem title={'Task 2'} creator={'Nam'} desc={'This a task This a task This a task This a task..'} />
+            <TodoItem title={'Task 3'} creator={'Vũ'} desc={'This a task This a task This a task This a task..'} />
+            <TodoItem title={'Task 4'} creator={'Mẫn'} desc={'This a task This a task This a task This a task..'} />
+        </div>
+    );
+}
+
+export default TodoList;
